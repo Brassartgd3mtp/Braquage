f848e3241b3ba65473d7f2732be5c890d6970520
Thomas CREUSOT <thomas.creusot@brassart.fr>
2023-10-19 16:22:03 +0200

    MainMenu Placeholder

---
+++ Assets/200_Scripts/280_UI/MainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("CoreScene");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Game Closed");
+        Application.Quit();
+    }
+}
