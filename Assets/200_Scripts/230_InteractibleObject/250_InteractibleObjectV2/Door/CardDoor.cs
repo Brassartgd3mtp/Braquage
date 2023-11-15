8de5d495fa20db3e4e48242d32894625cd8833d0
Thomas CREUSOT <thomas.creusot@brassart.fr>
2023-11-15 12:16:05 +0100

    LookAtCamera script + UI text added for doors
    - Création d'un script qui permet aux UI de progression de se tourner en direction de la camera (bug sur les CrochetDoor)
    - Ajout de texte indicatif sur les portes qui nécessitent une carte d'accès ou une foreuse
    - Nettoyage de la CoreScene (Rangement des mêmes éléments dans des empty + merge Canvas)
    - Integration des camera dans la CoreScene (il faut maintenant créer un computer qui prends en compte toutes les camera pour les désactiver)

--- Assets/200_Scripts/230_InteractibleObject/250_InteractibleObjectV2/Door/CardDoor.cs
+++ Assets/200_Scripts/230_InteractibleObject/250_InteractibleObjectV2/Door/CardDoor.cs
@@ -6,9 +6,12 @@ public class CardDoor : CrochetDoorV2
 {
     private bool FirstTime = false;
     public string NameCardAnimation;
+    public GameObject Text;
+
+    private bool playerNearby = false;
     void Start()
     {
-        
+        Text.gameObject.SetActive(false);
     }
 
 
@@ -21,6 +24,22 @@ public class CardDoor : CrochetDoorV2
                 doorAnimation.Play(NameCardAnimation);
                 FirstTime = true;
             }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNearby = true;
+            Text.gameObject.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNearby = false;
+            Text.gameObject.SetActive(false);
         }
     }
 }
