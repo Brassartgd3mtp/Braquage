503946a2f0194a13f6310cc3ee54e74ad3e2b858
Nicolas IMBERT <nicolas.imbert@brassart.fr>
2023-12-06 13:25:26 +0100

    Ajout path finder player

--- Assets/200_Scripts/220_Player/221_PlayerController/UnitMovement.cs
+++ Assets/200_Scripts/220_Player/221_PlayerController/UnitMovement.cs
@@ -2,8 +2,10 @@
 // Il permet à l'unité de se déplacer vers un point du sol lorsqu'un clic droit de la souris est effectué.
 
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Rendering;
 
 public class UnitMovement : MonoBehaviour
 {
@@ -19,33 +21,81 @@ public class UnitMovement : MonoBehaviour
 
     public static List<UnitMovement> unitMovementList = new List<UnitMovement>();
 
+    [SerializeField] private GameObject clickMarkerPrefab;
+    [SerializeField] private Transform visualObjectsParent;
+    [SerializeField] private LineRenderer myLineRenderer;
+
     void Start()
     {
         myCam = Camera.main;
         myAgent = GetComponent<NavMeshAgent>();
-        myAgent.speed = speed;
         animationController = GetComponent<AnimationController>();
+        myAgent.speed = speed;
 
-        enabled = false;
+        myLineRenderer = GetComponent<LineRenderer>();
+        myLineRenderer.startWidth = 0.15f;
+        myLineRenderer.endWidth = 0.15f;
+        myLineRenderer.positionCount = 0;
 
         unitMovementList.Add(this);
+
+
+        enabled = false;
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(1))
         {
-            RaycastHit hit;
-            Ray ray = myCam.ScreenPointToRay(Input.mousePosition);
+            ClickToMove();
+        }
 
-            // Effectue un raycast depuis la position de la souris vers le sol
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ground))
-            {
-                // Définit la destination de l'agent NavMesh vers le point touché par le raycast si le layer mask est ground
-                myAgent.SetDestination(hit.point);
+        if (myAgent.remainingDistance <= myAgent.stoppingDistance)
+        {
+            clickMarkerPrefab.transform.SetParent(transform);
+            clickMarkerPrefab.SetActive(false);
+        }
+        else if (myAgent.hasPath)
+        {
+            DrawPatch();
+        }
+    }
 
-                animationController.isWalking = true;
-            }
+    void ClickToMove()
+    {
+        RaycastHit hit;
+        Ray ray = myCam.ScreenPointToRay(Input.mousePosition);
+
+        // Effectue un raycast depuis la position de la souris vers le sol
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, ground))
+        {
+            SetDestination(hit.point);
+        }
+    }
+
+    void SetDestination(Vector3 target)
+    {
+        animationController.isWalking = true;
+        clickMarkerPrefab.transform.SetParent(visualObjectsParent);
+        clickMarkerPrefab.SetActive(true);
+        clickMarkerPrefab.transform.position = target;
+        myAgent.SetDestination(target);
+    }
+
+    void DrawPatch()
+    {
+        myLineRenderer.positionCount = myAgent.path.corners.Length;
+        myLineRenderer.SetPosition(0, transform.position);
+
+        if (myAgent.path.corners.Length < 2)
+        {
+            return;
+        }
+
+        for (int i = 1; i < myAgent.path.corners.Length; i++)
+        {
+            Vector3 pointPosition = new Vector3(myAgent.path.corners[i].x, myAgent.path.corners[i].y, myAgent.path.corners[i].z);
+            myLineRenderer.SetPosition(i, pointPosition);
         }
     }
 
